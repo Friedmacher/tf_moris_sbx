6675e20ffcd5cbd538c194b03998b566585e8ce4
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-07-18 20:19:04 +0000

    update: add ABAP

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 locals {
+  git_url        = "git::https://github.com/Friedmacher/tf_modules.git//modules/"
+  module_version = "v1.5.18"
   user_namess = [
-    "manuel.friedmacher@sap.com",
-    "manuel.friedmacher@outlook.com"
+    "manuel.friedmacher@sap.com"
   ]
 }
 
 module "sa_build" {
-  source            = "git::https://github.com/Friedmacher/tf_modules.git//modules/sa_build?ref=v1.4.7"
+  source            = "${local.git_url}sa_build?ref=${local.module_version}"
   project_name      = var.project_name
   subaccount_region = var.subaccount_region
   stage             = var.stage
@@ -14,35 +15,45 @@ module "sa_build" {
 }
 
 module "idp_trust" {
-  source        = "git::https://github.com/Friedmacher/tf_modules.git//modules/idp_trust?ref=v1.4.7"
+  source        = "${local.git_url}idp_trust?ref=${local.module_version}"
   subaccount_id = module.sa_build.subaccount_id
   btp_idp       = var.btp_idp
 }
 
 module "rc_assign" {
-  source           = "git::https://github.com/Friedmacher/tf_modules.git//modules/rc_assign?ref=v1.4.7"
+  source           = "${local.git_url}rc_assign?ref=${local.module_version}"
   subaccount_id    = module.sa_build.subaccount_id
   btp_platform_idp = var.btp_platform_idp
 }
 
 module "cf_enable" {
-  source               = "git::https://github.com/Friedmacher/tf_modules.git//modules/cf_enable?ref=v1.4.7"
+  source               = "${local.git_url}cf_enable?ref=${local.module_version}"
   subaccount_id        = module.sa_build.subaccount_id
   subaccount_subdomain = module.sa_build.subaccount_subdomain
   cf_region            = var.cf_region
 }
 
 module "cf_space_add" {
-  source        = "git::https://github.com/Friedmacher/tf_modules.git//modules/cf_space_add?ref=v1.4.7"
+  source        = "${local.git_url}cf_space_add?ref=${local.module_version}"
   cf_space_name = lower(var.project_name)
   cf_org_id     = module.cf_enable.cf_org_id
 }
 
 module "cf_user_add" {
-  source      = "git::https://github.com/Friedmacher/tf_modules.git//modules/cf_user_add?ref=v1.4.7"
+  source      = "${local.git_url}cf_user_add?ref=${local.module_version}"
   for_each    = toset(local.user_namess)
   user_name   = each.value
   idp_origin  = var.btp_platform_idp
   cf_org_id   = module.cf_enable.cf_org_id
   cf_space_id = module.cf_space_add.cf_space_id
+}
+
+module "abap_add" {
+  source                      = "${local.git_url}abap_add?ref=${local.module_version}"
+  subaccount_id               = module.sa_build.subaccount_id
+  cf_space_id                 = module.cf_space_add.cf_space_id
+  cf_region                   = var.cf_region
+  abap_sid                    = var.abap_sid
+  abap_admin_email            = var.abap_admin_email
+  abap_is_development_allowed = var.abap_is_development_allowed
 }
